e537279a366b6b49734613882eadd99cb3816f2f
manning1999 <manningcopeland2@gmail.com>
2020-07-24 13:18:55 +1000

    Converted the GlassGestureDetector class to C# so that gestures can be used in Unity Added a Plugins/Android/ folder which contains the custom manifest that allows the app to be added to the Google Glass's app menu Added a GyroCamera script which allows the camera to be controlled using the gyroscope (essentially turning the Glass into a lightweight HoloLens 1) Added a basic cube to the scene as a point of reference to showcase the GyroCamera Latest build and export (Exports can be opened in Android Studio) Application NEEDS to be exported and then opened and run by Android Studio in order to work properly because the custom manifest always seems to cause a crash when built and run directly from Unity

---
+++ Assets/Scripts/GestureDetector.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureDetector : MonoBehaviour
+{
+
+    public enum Gesture
+    {
+        TAP,
+        SWIPE_FORWARD,
+        SWIPE_BACKWARD,
+        SWIPE_UP,
+        SWIPE_DOWN,
+        NULL
+    }
+
+
+  
+
+    public Gesture gesture = Gesture.NULL;
+
+    public bool isTouched = false;
+
+    public interface OnGestureListener
+    {
+        bool onGesture(Gesture gesture);
+    }
+
+    private static int SWIPE_DISTANCE_THRESHOLD_PX = 100;
+    private static int SWIPE_VELOCITY_THRESHOLD_PX = 100;
+    private static double TAN_60_DEGREES = Mathf.Tan(Mathf.Deg2Rad * 60);
+
+    private GestureDetector gestureDetector;
+    private OnGestureListener onGestureListener;
+
+    Vector2 touchStartPosition;
+
+    public GestureDetector()
+    {
+       
+    }
+
+    void Update()
+    {
+
+        if (Input.touchCount == 1)
+        {
+            if(Input.touches[0].phase == TouchPhase.Began)
+            {
+                touchStartPosition = Input.touches[0].position;
+            }
+            if (Input.touches[0].phase == TouchPhase.Ended)
+            {
+                float velocityX = (Input.touches[0].deltaPosition.x - Input.touches[0].position.x) * Time.deltaTime;
+                float velocityY = (Input.touches[0].deltaPosition.y - Input.touches[0].position.y) * Time.deltaTime;
+
+
+                onFling(touchStartPosition, Input.touches[0].position, Input.touches[0].deltaPosition.x - Input.touches[0].position.x, Input.touches[0].deltaPosition.y - Input.touches[0].position.y);
+                
+            }
+            isTouched = true;
+           
+            
+                                                                                                
+        }
+        else
+        {
+            isTouched = false;
+            gesture = Gesture.NULL;
+        }
+
+
+    }
+
+
+
+    /**
+         * Swipe detection depends on the:
+         * - movement tan value,
+         * - movement distance,
+         * - movement velocity.
+         *
+         * To prevent unintentional SWIPE_DOWN and SWIPE_UP gestures, they are detected if movement
+         * angle is only between 60 and 120 degrees.
+         * Any other detected swipes, will be considered as SWIPE_FORWARD and SWIPE_BACKWARD, depends
+         * on deltaX value sign.
+         *
+         *           ______________________________________________________________
+         *          |                     \        UP         /                    |
+         *          |                       \               /                      |
+         *          |                         60         120                       |
+         *          |                           \       /                          |
+         *          |                             \   /                            |
+         *          |  BACKWARD  <-------  0  ------------  180  ------>  FORWARD  |
+         *          |                             /   \                            |
+         *          |                           /       \                          |
+         *          |                         60         120                       |
+         *          |                       /               \                      |
+         *          |                     /       DOWN        \                    |
+         *           --------------------------------------------------------------
+         */
+
+
+    public bool onFling(Vector2 startPos, Vector2 endPos, float velocityX, float velocityY)
+    {
+        float deltaX = endPos.x - startPos.x;
+        float deltaY = startPos.y - endPos.y;
+        double tan = deltaX != 0 ? Mathf.Abs(deltaY / deltaX) : double.MaxValue;
+
+        if (tan > TAN_60_DEGREES)
+        {
+            if (Mathf.Abs(deltaY) < SWIPE_DISTANCE_THRESHOLD_PX || Mathf.Abs(velocityY) < SWIPE_VELOCITY_THRESHOLD_PX)
+            {
+                // Application.Quit(0);
+                gesture = Gesture.TAP;
+                return false;
+            }
+            else if (deltaY < 0)
+            {
+                gesture = Gesture.SWIPE_UP;
+                return onGestureListener.onGesture(Gesture.SWIPE_UP);
+            }
+            else
+            {
+                gesture = Gesture.SWIPE_DOWN;
+                Application.Quit(0);
+                return onGestureListener.onGesture(Gesture.SWIPE_DOWN);
+            }
+        }
+        else
+        {
+            if (Mathf.Abs(deltaX) < SWIPE_DISTANCE_THRESHOLD_PX || Mathf.Abs(velocityX) < SWIPE_VELOCITY_THRESHOLD_PX)
+            {
+                gesture = Gesture.TAP;
+                return false;
+            }
+            else if (deltaX < 0)
+            {
+                gesture = Gesture.SWIPE_FORWARD;
+                transform.position += transform.forward * 3;
+                return onGestureListener.onGesture(Gesture.SWIPE_FORWARD);
+            }
+            else
+            {
+                gesture = Gesture.SWIPE_BACKWARD;
+                transform.position += transform.forward * -3;
+                return onGestureListener.onGesture(Gesture.SWIPE_BACKWARD);
+            }
+        }
+    }
+
+
+    public void OnGUI()
+    {
+        GUI.skin.label.fontSize = Screen.width / 40;
+        if (isTouched == true)
+        {
+            
+            GUILayout.Label(gesture.ToString());
+            GUILayout.Label("X: " + Input.touches[0].position.x);
+            GUILayout.Label("Y: " + Input.touches[0].position.y);
+            float deltaX = Input.touches[0].position.x - Input.touches[0].deltaPosition.x;
+            float deltaY = Input.touches[0].position.y - Input.touches[0].deltaPosition.y;
+            double tan = deltaX != 0 ? Mathf.Abs(deltaY / deltaX) : double.MaxValue;
+            GUILayout.Label("delta X: " + deltaX);
+            GUILayout.Label("delta Y: " + deltaY);
+            GUILayout.Label("Tan 60  deg: " + TAN_60_DEGREES);
+            GUILayout.Label("Tan: " + tan);
+        }
+        else
+        {
+            GUILayout.Label("Not Being Touched");
+        }
+    }
+}
+
---
+++ Assets/Scripts/GyroCameraController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroCameraController : MonoBehaviour
+{
+
+    Quaternion initialRotation = new Quaternion(0,0,0,0);
+    bool initSet = false;
+
+    Quaternion newAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GyroModifyCamera();
+    }
+
+
+    // The Gyroscope is right-handed.  Unity is left handed.
+    // Make the necessary change to the camera.
+    void GyroModifyCamera()
+    {
+        Input.gyro.enabled = true;
+        if (initialRotation.eulerAngles == new Quaternion(0,0,0,0).eulerAngles)
+        {
+            initialRotation = GyroToUnity(Input.gyro.attitude);
+            transform.parent.transform.rotation = Quaternion.Inverse(initialRotation);
+            initSet = true;
+        }
+
+        newAngle = GyroToUnity(Input.gyro.attitude);
+
+        transform.localRotation = newAngle;
+    }
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.z, q.y, -q.w);
+    }
+
+    /*
+
+    protected void OnGUI()
+    {
+        GUI.skin.label.fontSize = Screen.width / 40;
+
+        GUILayout.Label("Orientation: " + Screen.orientation);
+        GUILayout.Label("input.gyro.attitude: " + Input.gyro.attitude);
+        GUILayout.Label("iphone width/font: " + Screen.width + " : " + GUI.skin.label.fontSize);
+        GUILayout.Label("Initial Rotation: " + initialRotation);
+        GUILayout.Label("New Rotationn: " + newAngle);
+    }    */
+
+  
+
+    
+}
